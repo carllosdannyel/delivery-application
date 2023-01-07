c2fa6789b6c729e3089eca82870cbdc6f1648f7b
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2023-01-07 04:24:03 -0300

    adiciona e cria conexão com o express e mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
-const PORT = 3333;
 
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const port = 3001;
+    app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
+  })
+  .catch(() => console.log('erro ao conectar no mongodb'));
