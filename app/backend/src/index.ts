bf52472ad5b6c949cd8929daa924edb16b862b14
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2023-01-12 03:13:14 -0300

    adiciona socket.io e socket.io-client

--- app/backend/src/index.ts
+++ app/backend/src/index.ts
@@ -1,14 +1,19 @@
 import path from 'node:path';
+import http from 'node:http';
+import { Server } from 'socket.io';
 
 import express from 'express';
 import mongoose from 'mongoose';
 
 import { router } from './router';
 
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
+
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://localhost:27017')
   .then(() => {
-    const app = express();
     const port = 3001;
 
     app.use((_req, res, next) => {
@@ -21,6 +26,6 @@ mongoose.connect('mongodb://localhost:27017')
     app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
     app.use(express.json());
     app.use(router);
-    app.listen(port, () => console.log(`🚀 Server is running on http://localhost:${port}`));
+    server.listen(port, () => console.log(`🚀 Server is running on http://localhost:${port}`));
   })
   .catch(() => console.log('erro ao conectar no mongodb'));
